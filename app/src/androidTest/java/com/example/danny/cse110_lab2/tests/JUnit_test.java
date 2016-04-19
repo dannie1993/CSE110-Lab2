84e502aaf29d95c24a04f08bfbf8605c28f1db98
dannie1993 <dannie1993@gmail.com>
2016-04-19 15:38:21 -0700

    test_CL

--- app/src/androidTest/java/com/example/danny/cse110_lab2/tests/JUnit_test.java
+++ app/src/androidTest/java/com/example/danny/cse110_lab2/tests/JUnit_test.java
@@ -1,21 +1,26 @@
 package com.example.danny.cse110_lab2.tests;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
 import com.example.danny.cse110_lab2.MyActivity;
+import com.example.danny.cse110_lab2.R;
 
 /**
  * Created by danny on 4/19/16.
  */
 public class JUnit_test extends ActivityInstrumentationTestCase2<MyActivity> {
 
+    MyActivity myActivity;
     public JUnit_test(){
         super(MyActivity.class);
     }
 
-    public void test_first(){
-        MyActivity = getActivity();
 
-        TextView textView = (TextView) MyActivity.findViewByID(R.id.tv1);
-        String tester = textView.getText.toString();
+    public void test_first(){
+        myActivity = getActivity();
+
+        TextView textView = (TextView) myActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
 
         assertEquals("Bye World", tester);
 
